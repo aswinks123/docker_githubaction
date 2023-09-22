825181111bf957cc428c186e1e35cd2fd24aee47
aswin <aswinks123>
2023-09-22 03:13:11 -0400

    changed

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ app=Flask(__name__)
 
 def QR():
 
-    put_html(r"""<h1  align="center"><strong> CODE GENERATOR using GITHUB Actions</strong></h1>""")  # App Name in Main screen
+    put_html(r"""<h1  align="center"><strong> CODE GENERATOR CI/CD using GITHUB Actions</strong></h1>""")  # App Name in Main screen
     # Drop-down selection
 
 
